2f10372e6dfb29bd3a039ba149f4e051928fe211
IBT <ibt@Mac.fritz.box>
2026-06-24 17:48:36 +0200

    Update footer social links

--- app/data/social.ts
+++ app/data/social.ts
@@ -1,9 +1,11 @@
 export const socialLinks = [
   {
     label: "Instagram",
+    href: "https://www.instagram.com/getyour.design.info/",
   },
   {
     label: "Pinterest",
+    href: "https://www.pinterest.com/getyourdesignby2bhome/",
   },
   {
     label: "LinkedIn",
